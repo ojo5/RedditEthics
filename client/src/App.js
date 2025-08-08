48181ab85be3d48cd3ff9b4939fdc0d8f12920ba
ojo5 <109613345+ojo5@users.noreply.github.com>
2025-08-08 21:20:14 +0200

    React init
    Cleaned away the defaults

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial, sans-serif', textAlign: 'center' }}>
+    <h1>Ethics Learning Platform</h1>
+    <p>Welcome!</p>
+    </div>
+  );
+}
+
+export default App;
